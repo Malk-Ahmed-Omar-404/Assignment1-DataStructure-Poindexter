370ee1f3b19e31ee4ed9c83839abbaac0a84089b
Malk-Ahmed-Omar-404 <162677152+Malk-Ahmed-Omar-404@users.noreply.github.com>
2025-03-29 00:09:07 +0200

    New Feature
    Read from file

--- Problem 4/Problem4.cpp
+++ Problem 4/Problem4.cpp
@@ -1,53 +1,68 @@
 #include "SortingSystem.h"
 
 int main(){
+	string choice;
 	while (true) {
-		string n;
-		while (true) {
-			cout << "Enter the number of items to sort:";
-			getline(cin, n);
-			bool numCheck = true;
-			if (n.empty()) {
-				numCheck = false;
-			}
-			for (char ch : n) {
-				int v = ch;
-				if (!(ch >= 48 && ch <= 57)) {
+		cout << "1)Enter all inputs from a file\n2)Enter all inputs manually\nEnter your choice (1/2): ";
+		getline(cin, choice);
+		if (choice != "1" and choice != "2") {
+			cout << "Invalid choice. Please try again.\n";
+			continue;
+		}
+		if (choice == "1") {
+			SortingSystem<int> data(0);
+			data.readFromFile();
+			break;
+		}
+		else if (choice == "2") {
+			string n;
+			while (true) {
+				cout << "Enter the number of items to sort:";
+				getline(cin, n);
+				bool numCheck = true;
+				if (n.empty()) {
 					numCheck = false;
+				}
+				for (char ch : n) {
+					int v = ch;
+					if (!(ch >= 48 && ch <= 57)) {
+						numCheck = false;
+						break;
+					}
+				}
+				if (numCheck) {
 					break;
 				}
 			}
-			if (numCheck) {
+
+			string choice;
+			while (true) {
+				cout << "What is the type of data you are entering ?\n1)Integer\n2)Float\n3)Double\n4)String\nEnter your choice(1/2/3/4): ";
+				getline(cin, choice);
+				if (choice != "1" and choice != "2" and choice != "3" and choice != "4") {
+					cout << "Invalid choice. PLease try again.\n";
+					continue;
+				}
 				break;
 			}
-		}
 
-		string choice;
-		while (true) {
-			cout << "What is the type of data you are entering ?\n1)Integer\n2)Float\n3)Double\n4)String\nEnter your choice(1/2/3/4): ";
-			getline(cin, choice);
-			if (choice != "1" and choice != "2" and choice != "3" and choice != "4") {
-				cout << "Invalid choice. PLease try again.\n";
-				continue;
+			if (choice == "1") {
+				SortingSystem<int> data(stoi(n));
+				data.showMenu();
 			}
-			break;
+			else if (choice == "2") {
+				SortingSystem<float> data(stoi(n));
+				data.showMenu();
+			}
+			else if (choice == "3") {
+				SortingSystem<double> data(stoi(n));
+				data.showMenu();
+			}
+			else if (choice == "4") {
+				SortingSystem<string> data(stoi(n));
+				data.showMenu();
+			}
 		}
-	
-		if (choice == "1") {
-			SortingSystem<int> data(stoi(n));
-			data.showMenu();
-		}
-		else if (choice == "2") {
-			SortingSystem<float> data(stoi(n));
-			data.showMenu();
-		}
-		else if (choice == "3") {
-			SortingSystem<double> data(stoi(n));
-			data.showMenu();
-		}
-		else if (choice == "4") {
-			SortingSystem<string> data(stoi(n));
-			data.showMenu();}
 	}
 	return 0;
 }
--- Problem 4/SortingSystem.h
+++ Problem 4/SortingSystem.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 template <typename T>
@@ -26,6 +28,8 @@ public:
 
 	void displayData();
 	void measureSortTime(void(SortingSystem::*sortFunc)());
+	void readFromFile();
+	void setData(int i, T value);
 
 	void showMenu();
 	
@@ -71,6 +75,23 @@ template<typename T>
 void SortingSystem<T>::countSort() {}
 
 void SortingSystem<int>::countSort(){
+	if (size == 0) {
+		cout << "Your data is empty.It is already considered sorted.\n";
+		return;}
+	for (int i = 0; i < size; i++) {
+		if (data[i] < 0) {
+			cout << "Count sort can't be applied on negative values.\n";
+			return;
+		}
+	}
+	if (size == 1) {
+		cout << "Your data has only one element. It is already considered sorted.\n";
+		cout << "Initial data : ";
+		displayData();
+		cout << "Sorted data : ";
+		displayData();
+		return;
+	}
 	cout << "Initial data : ";
 	displayData();
 
@@ -117,6 +138,8 @@ void SortingSystem<int>::countSort(){
 	displayData();
 	delete[] countArray;
 	delete[] outputArray;
+	countArray= nullptr;
+	outputArray = nullptr;
 }
 
 template<typename T>
@@ -138,39 +161,51 @@ void SortingSystem<int>::radixSort(){
 		for (int i = 0; i < size; i++) {
 		tempData[i] = (data[i] / exp) % 10;}
 
-		cout << "Digits extracted for exp = " << exp << ": ";
+		cout << "Digits extracted for exp = " << exp << ": [";
 		for (int i = 0; i < size; i++) {
-			cout << tempData[i] << " ";
+			if (i != size - 1) cout << tempData[i] << " ";
+			else cout << tempData[i];
 		}
-		cout << endl;
+		cout << "]" << endl;
 
 		
 		int* countArray = new int[10]();
 
 		
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < size; i++) {
 			countArray[tempData[i]]++;
+		}
 
-		cout << "Count array after counting digits: ";
-		for (int i = 0; i < 10; i++) cout << countArray[i] << " ";
-		cout << endl;
+		cout << "Count array after counting digits: [";
+		for (int i = 0; i < 10; i++) {
+			if (i != 9) cout << countArray[i] << " ";
+			else cout << countArray[i];
+		}
+		cout << "]" << endl;
 		
-		for (int i = 1; i < 10; i++)
+		for (int i = 1; i < 10; i++) {
 			countArray[i] += countArray[i - 1];
+		}
 
 		
 		int* outputArray = new int[size]();
 		for (int i = size - 1; i >= 0; i--) {
 			outputArray[countArray[tempData[i]] - 1] = data[i];
 			countArray[tempData[i]]--;
-			cout << "Step " << size - i << ": ";
-			for (int j = 0; j < size; j++) cout << outputArray[j] << " ";
-			cout << endl;
+			cout << "Step " << size - i << ": [";
+			for (int j = 0; j < size; j++){
+				if (j != size - 1) cout << outputArray[j] << " ";
+				else cout << outputArray[j];
+			}
+			cout << "]" << endl;
 		}
 
-		cout << "Count array after accumulation: ";
-		for (int i = 0; i < 10; i++) cout << countArray[i] << " ";
-		cout << endl;
+		cout << "Count array after accumulation: [";
+		for (int i = 0; i < 10; i++) {
+			if (i != 9) cout << countArray[i] << " ";
+			else cout << countArray[i];
+		}
+		cout << "]" << endl;
 		
 		for (int i = 0; i < size; i++) {
 			data[i] = outputArray[i];
@@ -182,6 +217,9 @@ void SortingSystem<int>::radixSort(){
 		delete[] tempData;
 		delete[] countArray;
 		delete[] outputArray;
+		tempData = nullptr;
+		countArray = nullptr;
+		outputArray = nullptr;
 	}
 	cout << "Sorted data : ";
 	displayData();
@@ -225,6 +263,148 @@ void SortingSystem<T>::measureSortTime(void(SortingSystem::* sortFunc)()){
 	auto end = chrono::high_resolution_clock::now();
 	chrono::duration<double> duration = end - start;
 	cout << "Sorting Time: " << duration.count() << " seconds" << endl;
+}
+
+template<typename T>
+void SortingSystem<T>::readFromFile(){
+	string filename;
+	ifstream inputFile;
+	while (true) {
+		string ch;
+		cout << "Use the Testcases default file? (enter Y or N):";
+		getline(cin, ch);
+		if (ch == "y" or ch == "Y") {
+			inputFile.open("TestcasesPb4.txt", ios::in);
+			filename = "TestcasesPb4.txt";
+			break;
+		}
+		else if (ch == "n" or ch == "N") {
+			while (true) {
+				cout << "Enter the name of the file (with extension): ";
+				getline(cin, filename);
+				inputFile.open(filename);
+				if (inputFile.is_open()) {
+					break;
+				}
+				cout << "Error opening the file!\nTry entering the file name again.\n";
+			}
+			break;
+		}
+		else {
+			cout << "Please enter a valid choice.\n";
+			continue;
+		}
+	}
+	int counter = 1;
+	string numElements, dataType, dataInput, choice, continueOP;
+	while (getline(inputFile, numElements) && getline(inputFile, dataType) && getline(inputFile, dataInput) && getline(inputFile, choice) && getline(inputFile, continueOP)) {
+		cout << "Testcase #" << counter << endl;
+		int size = stoi(numElements);
+		cout << "Number of elements: " << size << endl;
+
+		stringstream dataStream(dataInput);
+
+		if (dataType == "1") {
+			cout << "Data type: Integer" << endl;
+			SortingSystem<int> arr(size);
+			for (int i = 0; i < size; i++) {
+				int value;
+				dataStream >> value;
+				arr.setData(i, value);}
+
+			cout << "Data loaded :";
+			arr.displayData();
+
+			if (choice == "1") arr.measureSortTime(&SortingSystem<int>::insertionSort);
+			else if (choice == "2") arr.measureSortTime(&SortingSystem<int>::selectionSort);
+			else if (choice == "3") arr.measureSortTime(&SortingSystem<int>::bubbleSort);
+			else if (choice == "4") arr.measureSortTime(&SortingSystem<int>::shellSort);
+			else if (choice == "5") cout << "Nothing to display\n";
+			else if (choice == "6") cout << "Nothing to display\n";
+			else if (choice == "7") arr.measureSortTime(&SortingSystem<int>::countSort);
+			else if (choice == "8") arr.measureSortTime(&SortingSystem<int>::radixSort);
+			else if (choice == "9") arr.measureSortTime(&SortingSystem<int>::bucketSort);
+		}
+		else if (dataType == "2") {
+			cout << "Data type: Float" << endl;
+			SortingSystem<float> arr(size);
+			for (int i = 0; i < size; i++) {
+				float value;
+				dataStream >> value;
+				arr.setData(i, value);
+			}
+			cout << "Data loaded :";
+			arr.displayData();
+
+			if (choice == "1") arr.measureSortTime(&SortingSystem<float>::insertionSort);
+			else if (choice == "2") arr.measureSortTime(&SortingSystem<float>::selectionSort);
+			else if (choice == "3") arr.measureSortTime(&SortingSystem<float>::bubbleSort);
+			else if (choice == "4") arr.measureSortTime(&SortingSystem<float>::shellSort);
+			else if (choice == "5") cout << "Nothing to display\n";
+			else if (choice == "6") cout << "Nothing to display\n";
+			else if (choice == "7") cout << "Count sort can't be applied on Float values.\n";
+			else if (choice == "8") cout << "Radix sort can't be applied on Float values.\n";
+			else if (choice == "9") arr.measureSortTime(&SortingSystem<float>::bucketSort);
+
+		}
+		else if (dataType == "3") {
+			cout << "Data type: Double" << endl;
+			SortingSystem<double> arr(size);
+			for (int i = 0; i < size; i++) {
+				double value;
+				dataStream >> value;
+				arr.setData(i, value);
+			}
+			cout << "Data loaded: ";
+			arr.displayData();
+
+			if (choice == "1") arr.measureSortTime(&SortingSystem<double>::insertionSort);
+			else if (choice == "2") arr.measureSortTime(&SortingSystem<double>::selectionSort);
+			else if (choice == "3") arr.measureSortTime(&SortingSystem<double>::bubbleSort);
+			else if (choice == "4") arr.measureSortTime(&SortingSystem<double>::shellSort);
+			else if (choice == "5") cout << "Nothing to display\n";
+			else if (choice == "6") cout << "Nothing to display\n";
+			else if (choice == "7") cout << "Count sort can't be applied on Double values.\n";
+			else if (choice == "8") cout << "Radix sort can't be applied on Double values.\n";
+			else if (choice == "9") arr.measureSortTime(&SortingSystem<double>::bucketSort);
+		}
+		else if (dataType == "4") {
+			cout << "Data type: String" << endl;
+			SortingSystem<string> arr(size);
+			for (int i = 0; i < size; i++) {
+				string value;
+				dataStream >> value;
+				arr.setData(i, value);
+			}
+			cout << "Data loaded: ";
+			arr.displayData();
+
+			if (choice == "1") arr.measureSortTime(&SortingSystem<string>::insertionSort);
+			else if (choice == "2") arr.measureSortTime(&SortingSystem<string>::selectionSort);
+			else if (choice == "3") arr.measureSortTime(&SortingSystem<string>::bubbleSort);
+			else if (choice == "4") arr.measureSortTime(&SortingSystem<string>::shellSort);
+			else if (choice == "5") cout << "Nothing to display\n";
+			else if (choice == "6") cout << "Nothing to display\n";
+			else if (choice == "7") cout << "Count sort can't be applied on String values.\n";
+			else if (choice == "8") cout << "Radic sort can't be applied on String values.\n";
+			else if (choice == "9") arr.measureSortTime(&SortingSystem<string>::bucketSort);
+		}
+		counter++;
+		cout << "-----------------------------------------------------------------------------------------------------------------------\n";
+		if (continueOP == "n" || continueOP == "N") {
+			cout << "Thank you for using the Statistical Calculation system! Goodbye!";
+			break;
+		}
+	}
+
+	inputFile.close();
+}
+
+template<typename T>
+void SortingSystem<T>::setData(int i, T value){
+	if (i >= 0 && i < size) {
+		data[i] = value;
+	}
 }
 
 template<typename T>
@@ -279,10 +459,8 @@ void SortingSystem<T>::showMenu(){
 				measureSortTime(&SortingSystem::shellSort);
 			}
 			else if (choice == "5") {
-				//measureSortTime(&SortingSystem::mergeSort);
 			}
 			else if (choice == "6") {
-				//measureSortTime(&SortingSystem::quickSort);
 			}
 			else if (choice == "7") {
 				if (!is_same<T, int>::value) {
@@ -322,6 +500,7 @@ void SortingSystem<T>::showMenu(){
 			break;
 		}
 		if (choice == "y") {
+			cout << "-----------------------------------------------------------------------------------------------------------------------\n";
 			break;}
 		else if (choice == "n") {
 			cout << "Thank you for using the sorting system! Goodbye!"; 
